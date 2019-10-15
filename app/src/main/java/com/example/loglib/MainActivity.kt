be6a11b9d2e320f1eb4e6ec4058cbc36968c5b1e
MikeAde1 <michaeladeneye1@gmail.com>
2019-10-15 13:31:37 +0100

    Used log debug library and Log error library

--- app/src/main/java/com/example/loglib/MainActivity.kt
+++ app/src/main/java/com/example/loglib/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.loglib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logerror.LogError
+import com.example.loglibrary.LogDebug
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LogDebug.setDebugLog("arrived")
+        LogError.setErrorLog("Error Arriving")
     }
 }
